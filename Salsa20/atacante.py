81b4cee5ef40c2df017ade1afc9be1bba1d2093e
Sebastian Arteta Padilla <98917806+sebastianpadilla02@users.noreply.github.com>
2024-10-13 02:24:18 +0000

    falta seguir con pasos bebe pasos gigante

--- Salsa20/atacante.py
+++ Salsa20/atacante.py
@@ -1,19 +1,55 @@
 import yaml
-from Salsa20.Dif_Hel_2.funciones import Crypto_functions
+from funciones import Crypto_functions
+import json
+import math
 
-def leer_y_extraer_mensajes_yaml(ruta_archivo):
+y = None
+
+def pasos_bebe_pasos_gigante(g, y, p):  #g:Generador conocido, y: u o v capturado, n: p-1)
+    m = math.ceil(math.sqrt(p-1))
+
+    R = {}
+    # Paso de bebé: Precalcular los valores de g^j mod p y almacenarlos en un diccionario
+    for j in range(m):
+        R[pow(g, j, p)] = j
+
+    # Paso de gigante: Calcular g^(-m) mod p
+    B = pow(g, -m, p)
+
+    Y = y
+
+    for i in range(m):
+        if Y in R:
+            x = i * m + R[Y]
+            return x
+        
+        Y = (Y * B) % p
+
+    return None
+
+
+def leer_y_extraer_mensajes_yaml(ruta_archivo, p, q, g):
     with open(ruta_archivo, 'r') as file:
         contenido_yaml = yaml.safe_load(file)  # Leer y cargar el archivo YAML
 
-    llave = False
+    publico = False
+    privado = False
     # Iterar sobre los paquetes y extraer los datos
     for packet in contenido_yaml['packets']:
         datos_base64 = packet['data']  # Extraer el dato en base64
-        if(llave == False):
-            key = datos_base64
-            llave = True
+        if(publico == False):
+            u = datos_base64
+            publico = True
+
+            x = pasos_bebe_pasos_gigante(g, u, p)
+            if(x == None):
+                print("No se pudo encontrar el valor de alpha")
+                return None
             continue
 
+        if (privado == False):
+            
+            privado = True
         # Extraer el nonce del mensaje
         nonce = datos_base64[:8]  # Asumimos que el nonce es de 8 bytes
         encrypted_message = datos_base64[8:]
@@ -25,8 +61,22 @@ def leer_y_extraer_mensajes_yaml(ruta_archivo):
         else:
             print(f"Cliente: {desencriptado.decode('utf-8')}")
             
+# Cargar el archivo JSON con los parámetros p, q, g
+with open('parameters.json') as f:
+    data = json.load(f)
+
+# Acceder a un único conjunto de parámetros, por ejemplo, el primero
+param_set = data["parameters"][0]  # Cambia el índice a 0, 1, 2... según el conjunto que quieras
+
+# Acceder a los valores de p, q y g
+p = param_set["p"]
+q = param_set["q"]
+g = param_set["g"]
+
+print(f"Conjunto seleccionado: p={p}, q={q}, g={g}")
+
 # Ruta del archivo YAML
 ruta_archivo_yaml = 'data.yaml'  # Cambia esto con la ruta correcta
 
 # Leer y extraer mensajes del archivo YAML
-leer_y_extraer_mensajes_yaml(ruta_archivo_yaml)
+leer_y_extraer_mensajes_yaml(ruta_archivo_yaml, p, q ,g)
---
+++ prueba.py
@@ -0,0 +1,6 @@
+import math
+
+n = 121
+m = math.ceil(math.sqrt(n))
+
+print(f"El valor de m es: {m}")
